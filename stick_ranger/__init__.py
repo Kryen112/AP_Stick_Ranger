6a0a2808db8f03837310aa3d086d6d45b0fdc92b
Kryen112 <Kryen112@hotmail.com>
2025-05-14 17:11:59 +0200

    Revert init refactor + levelups (TODO: implement levelups and then refactor)

--- stick_ranger/__init__.py
+++ stick_ranger/__init__.py
@@ -1,35 +1,11 @@
 import random
-from BaseClasses import Region, Entrance
+from BaseClasses import Region, Item, Entrance
 from worlds.AutoWorld import World
-from .Items import SRItem, item_table, stages, levelups, filler, traps
-from .Locations import SRLocation, stages_table, books_table, enemies_table, levelups_table, location_name_to_id
+from .Items import SRItem, item_table, stages, filler, traps
+from .Locations import SRLocation, stages_table, books_table, enemies_table, location_name_to_id
 from .Options import SROptions
 from .Regions import regions
 from .Rules import set_rules
-
-
-ENEMIES_NORMAL = 1
-ENEMIES_BOSS = 2
-ENEMIES_ALL = 3
-
-TRAP_STEP_PERCENT = 25
-
-STARTER_UNLOCK_CHOICES = [
-    "Unlock Grassland 1",
-    "Unlock Grassland 2",
-    "Unlock Grassland 3",
-    "Unlock Grassland 4",
-    "Unlock Hill Country 1"
-]
-OPENING_STREET_EXIT = "Opening Street: Exit"
-OPENING_STREET_BOOK = "Opening Street: Book"
-OPENING_STREET_ENEMIES = [
-    "Opening Street: Green Smiley Walker",
-    "Opening Street: Cyan Smiley Walker",
-    "Opening Street: Red Smiley Walker",
-    "Opening Street: Blue X Walker"
-]
-OPENING_STREET_BOSS = "Opening Street: Grey Boss Smiley Walker"
 
 class StickRanger(World):
     game = "Stick Ranger"
@@ -40,7 +16,6 @@ class StickRanger(World):
     item_name_to_id = { name: data.code for name, data in item_table.items() }
     start_inventory = {}
 
-
     def create_regions(self):
         menu_region = Region("Menu", self.player, self.multiworld)
         world_map_region = Region("World Map", self.player, self.multiworld)
@@ -49,51 +24,66 @@ class StickRanger(World):
         menu_region.exits.append(menu_to_world_map_exit)
         menu_to_world_map_exit.connect(world_map_region)
 
-        def filter_locations(table, region, filter_func=None):
-            return {
-                loc["name"]: loc_id
-                for loc_id, loc in table.items()
-                if loc["region"] == region and (filter_func(loc) if filter_func else True)
-            }
-
-        def make_unlock_rule(region_name):
-            return lambda state: state.has(f"Unlock {region_name}", self.player)
-
+        # Create regions and locations
         for region_name in regions:
             region = Region(region_name, self.player, self.multiworld)
 
-            stage_locs = filter_locations(stages_table, region_name)
+            # 1) always add stage‐exits
+            stage_locs = {
+                loc["name"]: loc_id
+                for loc_id, loc in stages_table.items()
+                if loc["region"] == region_name
+            }
             region.add_locations(stage_locs, SRLocation)
 
             # TODO Make sure either books, enemies or levelups is selected!
+            # 2) optionally add books
             if self.options.shuffle_books == 1:
-                book_locs = filter_locations(books_table, region_name)
+                book_locs = {
+                    loc["name"]: loc_id
+                    for loc_id, loc in books_table.items()
+                    if loc["region"] == region_name
+                }
                 region.add_locations(book_locs, SRLocation)
 
-            enemy_filters = {
-                ENEMIES_NORMAL: lambda loc: "boss" not in loc["name"].lower(),
-                ENEMIES_BOSS: lambda loc: "boss" in loc["name"].lower(),
-                ENEMIES_ALL: None
-            }
-
-            enemy_locations = filter_locations(enemies_table, region_name, enemy_filters.get(self.options.shuffle_enemies))
-            region.add_locations(enemy_locations, SRLocation)
+            # 3) optionally add enemies
+            shuffle_enemies = self.options.shuffle_enemies
+            if shuffle_enemies == 1:
+                # only non‐boss enemies
+                enemy_locs = {
+                    loc["name"]: loc_id
+                    for loc_id, loc in enemies_table.items()
+                    if loc["region"] == region_name
+                       and "boss" not in loc["name"].lower()
+                }
+                region.add_locations(enemy_locs, SRLocation)
+            elif shuffle_enemies == 2:
+                # only bosses
+                boss_locs = {
+                    loc["name"]: loc_id
+                    for loc_id, loc in enemies_table.items()
+                    if loc["region"] == region_name
+                       and "boss" in loc["name"].lower()
+                }
+                region.add_locations(boss_locs, SRLocation)
+            elif shuffle_enemies == 3:
+                # all enemies
+                all_enemy_locs = {
+                    loc["name"]: loc_id
+                    for loc_id, loc in enemies_table.items()
+                    if loc["region"] == region_name
+                }
+                region.add_locations(all_enemy_locs, SRLocation)
 
             self.multiworld.regions.append(region)
 
             world_map_exit = Entrance(self.player, region_name, world_map_region)
             if region_name != "Opening Street":
-                world_map_exit.access_rule = make_unlock_rule(region_name)
+                world_map_exit.access_rule = lambda st, item=f"Unlock {region_name}": st.has(item, self.player)
             world_map_region.exits.append(world_map_exit)
             world_map_exit.connect(region)
 
-        if self.options.shuffle_levelups:
-            levelup_locs = {
-                loc["name"]: loc_id
-                for loc_id, loc in levelups_table.items()
-            }
-            world_map_region.add_locations(levelup_locs, SRLocation)
-
+        # Sum locations for items creation
         self.location_count = len(self.multiworld.get_locations(self.player))
 
     def create_item(self, name: str) -> SRItem:
@@ -101,33 +91,23 @@ class StickRanger(World):
         return SRItem(name, item_data.classification, item_data.code, self.player)
 
     def create_items(self):
-        starter_item_name = random.choice(STARTER_UNLOCK_CHOICES)
+        starter_choices = [
+            "Unlock Grassland 1",
+            "Unlock Grassland 2",
+            "Unlock Grassland 3",
+            "Unlock Grassland 4",
+            "Unlock Hill Country 1"
+        ]
+        starter_item_name = random.choice(starter_choices)
         starter_item = self.create_item(starter_item_name)
-
-        starter_location_names = [OPENING_STREET_EXIT]
-        if self.options.shuffle_books == 1:
-            starter_location_names.append(OPENING_STREET_BOOK)
-        shuffle_enemies = self.options.shuffle_enemies
-        if shuffle_enemies in (1, 3):
-            starter_location_names.extend(OPENING_STREET_ENEMIES)
-        if shuffle_enemies in (2, 3):
-            starter_location_names.append(OPENING_STREET_BOSS)
-        if self.options.shuffle_levelups:
-            levelup_count = 98
-            self.multiworld.itempool += [
-                self.create_item(level.item_name)
-                for level in levelups[:levelup_count]
-            ]
-
-        random_loc_name = random.choice(starter_location_names) # keep in mind: level shuffle
-        starter_loc = self.multiworld.get_location(random_loc_name, self.player)
+        starter_loc_name = random.choice(["Opening Street Exit", "Opening Street Book"])
+        starter_loc = self.multiworld.get_location(starter_loc_name, self.player)
         starter_loc.place_locked_item(starter_item)
 
-        self.multiworld.itempool += [
-            self.create_item(unlock.item_name)
-            for unlock in stages
-            if unlock.item_name != starter_item_name
-        ]
+        for level in stages:
+            if level.item_name == starter_item_name:
+                continue
+            self.multiworld.itempool.append(self.create_item(level.item_name))
 
     def pre_fill(self):
         missing_locs = len(self.multiworld.get_unfilled_locations()) - len(self.multiworld.itempool)
@@ -137,13 +117,13 @@ class StickRanger(World):
         traps_option = self.options.traps
         traps_percentage = 0
         if traps_option >= 1:
-            traps_percentage = traps_option * TRAP_STEP_PERCENT
+            traps_percentage = 25 * traps_option
 
         trap_count = int((traps_percentage / 100) * missing_locs)
         trap_weights = [trap.weight for trap in traps]
-        for _ in range(trap_count):
-            trap = random.choices(traps, weights=trap_weights, k=1)[0]
-            self.multiworld.itempool.append(self.create_item(trap.item_name))
+        self.multiworld.itempool += [
+            self.create_item(random.choices(traps, weights=trap_weights, k=1)[0].item_name) for _ in range(trap_count)
+        ]
 
         filler_count = missing_locs - trap_count
         self.multiworld.itempool += [
